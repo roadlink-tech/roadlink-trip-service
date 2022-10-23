afa2cd8993aa9e72ca551ee7bce9f3357432b14d
jorgejcabrera <cabrerajjorge@gmail.com>
2022-10-22 23:01:44 -0300

    adding healthcheck

---
+++ src/main/kotlin/com/fdt/tripservice/application/rest/HealthCheckController.kt
@@ -0,0 +1,14 @@
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class HealthController {
+
+    @GetMapping
+    fun healthCheck(): String {
+        return "ok"
+    }
+}
