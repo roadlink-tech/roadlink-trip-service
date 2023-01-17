d1969d2db70bb91ce23c8a366d8fd8b1bc096c4b
mbosch <martinbosch17@gmail.com>
2023-01-16 21:45:56 -0300

    Change latitude and longitude of fixed section in the repository

--- src/main/kotlin/com/roadlink/tripservice/config/InMemorySectionRepositoryConfig.kt
+++ src/main/kotlin/com/roadlink/tripservice/config/InMemorySectionRepositoryConfig.kt
@@ -15,7 +15,11 @@ class InMemorySectionRepositoryConfig {
         return InMemorySectionRepository(sections = mutableListOf(
                 Section(
                     departure = TripPoint(
-                        location = Location(latitude = -34.540412, longitude = -58.474732, alias = "AvCabildo 4853"),
+                        location = Location(
+                            latitude = -34.54025770408163,
+                            longitude = -58.47450726734694,
+                            alias = "AvCabildo 4853",
+                        ),
                         at = Instant.parse("2022-10-15T12:00:00Z"),
                         formatted = "Av. Cabildo 4853, Buenos Aires",
                         street = "Av. Cabildo",
@@ -24,7 +28,11 @@ class InMemorySectionRepositoryConfig {
                         housenumber = "4853",
                     ),
                     arrival = TripPoint(
-                        location = Location(latitude = -34.574810, longitude = -58.435990, alias = "AvCabildo 20"),
+                        location = Location(
+                            latitude = -34.57489533621165,
+                            longitude = -58.435972139652776,
+                            alias = "AvCabildo 20",
+                        ),
                         at = Instant.parse("2022-10-15T13:00:00Z"),
                         formatted = "Av. Cabildo 20, Buenos Aires",
                         street = "Av. Cabildo",
