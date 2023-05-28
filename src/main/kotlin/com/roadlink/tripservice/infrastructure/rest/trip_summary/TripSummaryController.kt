178fa23fca105d4eebca4d933a76ef80a4256b27
jorgecabrera <cabrerajjorge@gmail.com>
2023-05-27 22:56:35 -0300

    basic json response

---
+++ src/main/kotlin/com/roadlink/tripservice/infrastructure/rest/trip_summary/TripSummaryController.kt
@@ -0,0 +1,22 @@
+package com.roadlink.tripservice.infrastructure.rest.trip_summary
+
+
+/*
+* JSONRESPONSE
+* {
+      [
+        {
+          "id": Int,
+          "departure_district": String,
+          "departure_instant": Instant,
+          "arrival_district": String,
+          "arrival_instant": Instant,
+    	  "trip_status": String,
+	      "has_seat_available": Boolean,
+          "has_pending_notifications": Boolean
+        }
+      ]
+   }
+   * */
+class TripSummaryController {
+}
