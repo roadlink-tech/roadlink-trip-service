951a29192d4cd24d06f014602d01b047036eefc2
mbosch <martinbosch17@gmail.com>
2023-09-11 21:18:51 -0300

    foo

--- src/main/kotlin/com/roadlink/tripservice/infrastructure/rest/handlers/HealthCheckRestController.kt
+++ src/main/kotlin/com/roadlink/tripservice/infrastructure/rest/handlers/HealthCheckRestController.kt
@@ -1,9 +1,12 @@
 package com.roadlink.tripservice.infrastructure.rest.handlers
 
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
+import jakarta.inject.Singleton
 
 @Controller("/health")
 class HealthCheckRestController {
@@ -11,5 +14,26 @@ class HealthCheckRestController {
     @Produces(MediaType.TEXT_PLAIN)
     fun handle(): String {
         return "ok"
+    }
+}
+
+@Factory
+class FooConfig {
+
+    @Value("\${foo}")
+    private lateinit var foo: String
+
+    @Singleton
+    fun foo() = Foo(foo)
+}
+
+class Foo(val value: String)
+
+@Controller("/foo")
+class FooRestController(private val foo: Foo) {
+    @Get
+    @Produces(MediaType.TEXT_PLAIN)
+    fun handle(): String {
+        return foo.value
     }
 }
