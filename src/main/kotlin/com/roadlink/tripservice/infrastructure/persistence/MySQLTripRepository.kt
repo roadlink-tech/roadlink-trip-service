582f2efe59144519fa700eb4e6044305c1699f44
mbosch <martinbosch17@gmail.com>
2023-11-06 21:13:33 -0300

    WIP

--- src/main/kotlin/com/roadlink/tripservice/infrastructure/persistence/MySQLTripRepository.kt
+++ src/main/kotlin/com/roadlink/tripservice/infrastructure/persistence/MySQLTripRepository.kt
@@ -3,7 +3,7 @@ package com.roadlink.tripservice.infrastructure.persistence
 import com.roadlink.tripservice.domain.time.TimeRange
 import com.roadlink.tripservice.domain.trip.Trip
 import com.roadlink.tripservice.domain.trip.TripRepository
-import jakarta.persistence.*
+import jakarta.persistence.EntityManager
 import java.util.*
 
 class MySQLTripRepository(
@@ -14,7 +14,23 @@ class MySQLTripRepository(
     }
 
     override fun existsByDriverAndInTimeRange(driver: String, timeRange: TimeRange): Boolean {
-        TODO("Not yet implemented")
+        return entityManager.createQuery(
+        """
+            |SELECT t 
+            |FROM TripJPAEntity t
+            |WHERE 
+            |   t.driver = :driverId
+            |   AND NOT :to <= t.departure.estimatedArrivalTime
+            |   AND NOT :from >= t.arrival.estimatedArrivalTime
+            |""".trimMargin(),
+        TripJPAEntity::class.java
+    )
+        .setParameter("driverId", driver)
+        .setParameter("to", timeRange.to)
+        .setParameter("from", timeRange.from)
+        .resultList
+        .map { it.toDomain() }
+        .isNotEmpty()
     }
 
     override fun findAllByDriverId(driverId: UUID): List<Trip> {
@@ -31,68 +47,4 @@ class MySQLTripRepository(
             .map { it.toDomain() }
     }
 
-}
-
-@Entity
-@Table(name = "trip")
-data class TripJPAEntity(
-    @Id val id: String,
-    val driver: String,
-    val vehicle: String,
-    @Embedded
-    @AttributeOverrides(
-        AttributeOverride(name = "estimatedArrivalTime", column = Column(name = "departure_estimated_arrival_time")),
-        AttributeOverride(name = "fullAddress", column = Column(name = "departure_full_address")),
-        AttributeOverride(name = "street", column = Column(name = "departure_street")),
-        AttributeOverride(name = "city", column = Column(name = "departure_city")),
-        AttributeOverride(name = "country", column = Column(name = "departure_country")),
-        AttributeOverride(name = "housenumber", column = Column(name = "departure_housenumber")),
-        AttributeOverride(name = "latitude", column = Column(name = "departure_latitude")),
-        AttributeOverride(name = "longitude", column = Column(name = "departure_longitude"))
-    )
-    val departure: TripPointJPAEntity,
-    @Embedded
-    @AttributeOverrides(
-        AttributeOverride(name = "estimatedArrivalTime", column = Column(name = "arrival_estimated_arrival_time")),
-        AttributeOverride(name = "fullAddress", column = Column(name = "arrival_full_address")),
-        AttributeOverride(name = "street", column = Column(name = "arrival_street")),
-        AttributeOverride(name = "city", column = Column(name = "arrival_city")),
-        AttributeOverride(name = "country", column = Column(name = "arrival_country")),
-        AttributeOverride(name = "housenumber", column = Column(name = "arrival_housenumber")),
-        AttributeOverride(name = "latitude", column = Column(name = "arrival_latitude")),
-        AttributeOverride(name = "longitude", column = Column(name = "arrival_longitude"))
-    )
-    val arrival: TripPointJPAEntity,
-    @ElementCollection
-    val meetingPoints: List<TripPointJPAEntity>,
-    val status: String,
-    val availableSeats: Int,
-) {
-    companion object {
-        fun from(trip: Trip): TripJPAEntity {
-            return TripJPAEntity(
-                id = trip.id,
-                driver = trip.driver,
-                vehicle = trip.vehicle,
-                departure = TripPointJPAEntity.from(trip.departure),
-                arrival = TripPointJPAEntity.from(trip.arrival),
-                meetingPoints = trip.meetingPoints.map { TripPointJPAEntity.from(it) },
-                status = trip.status.name,
-                availableSeats = trip.availableSeats,
-            )
-        }
-    }
-
-    fun toDomain(): Trip {
-        return Trip(
-            id = id,
-            driver = driver,
-            vehicle = vehicle,
-            departure = departure.toDomain(),
-            arrival = arrival.toDomain(),
-            meetingPoints = meetingPoints.map { it.toDomain() },
-            status = Trip.Status.valueOf(status),
-            availableSeats = availableSeats,
-        )
-    }
 }
