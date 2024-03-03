001a6b7c48213533b284576b32ced0eb784b8d4f
Martin Bosch <martinbosch17@gmail.com>
2024-03-03 20:53:08 -0300

    feat: get user and rating from core repository (#17)

--- src/main/kotlin/com/roadlink/tripservice/config/RatingRepositoryConfig.kt
+++ src/main/kotlin/com/roadlink/tripservice/config/RatingRepositoryConfig.kt
@@ -1,14 +1,45 @@
 package com.roadlink.tripservice.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.roadlink.tripservice.dev_tools.infrastructure.network.Get
 import com.roadlink.tripservice.domain.RatingRepository
 import com.roadlink.tripservice.infrastructure.persistence.FixedRatingRepository
+import com.roadlink.tripservice.infrastructure.remote.HttpRatingRepository
 import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Value
 import jakarta.inject.Singleton
+import org.slf4j.LoggerFactory
 
 @Factory
 class RatingRepositoryConfig {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(RatingRepositoryConfig::class.java)
+    }
+
+    @Value("\${http-get-user-trust-score.scheme}")
+    private lateinit var getUserTrustScoreScheme: String
+
+    @Value("\${http-get-user-trust-score.host}")
+    private lateinit var getUserTrustScoreHost: String
+
+    @Value("\${http-get-user-trust-score.port}")
+    private var getUserTrustScorePort: Int? = null
+
+    @Value("\${http-get-user-trust-score.endpoint}")
+    private lateinit var getUserTrustScoreEndpoint: String
+
     @Singleton
-    fun ratingRepository(): RatingRepository {
-        return FixedRatingRepository()
+    fun ratingRepository(get: Get, objectMapper: ObjectMapper): RatingRepository {
+        logger.debug("Initializing http get user trust score with scheme: {}, host: {}, port: {}, endpoint: {}", getUserTrustScoreScheme, getUserTrustScoreHost, getUserTrustScorePort, getUserTrustScoreEndpoint)
+
+        return HttpRatingRepository(
+            get = get,
+            scheme = getUserTrustScoreScheme,
+            host = getUserTrustScoreHost,
+            port = getUserTrustScorePort!!,
+            endpoint = getUserTrustScoreEndpoint,
+            objectMapper = objectMapper,
+        )
     }
 }
--- src/main/kotlin/com/roadlink/tripservice/config/UserRepositoryConfig.kt
+++ src/main/kotlin/com/roadlink/tripservice/config/UserRepositoryConfig.kt
@@ -1,14 +1,46 @@
 package com.roadlink.tripservice.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.roadlink.tripservice.dev_tools.config.HttpGeoapifyConfig
+import com.roadlink.tripservice.dev_tools.infrastructure.network.Get
 import com.roadlink.tripservice.domain.user.UserRepository
 import com.roadlink.tripservice.infrastructure.persistence.user.FixedUserRepository
+import com.roadlink.tripservice.infrastructure.remote.HttpUserRepository
 import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Value
 import jakarta.inject.Singleton
+import org.slf4j.LoggerFactory
 
 @Factory
 class UserRepositoryConfig {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(UserRepositoryConfig::class.java)
+    }
+
+    @Value("\${http-get-user-by-id.scheme}")
+    private lateinit var getUserByIdScheme: String
+
+    @Value("\${http-get-user-by-id.host}")
+    private lateinit var getUserByIdHost: String
+
+    @Value("\${http-get-user-by-id.port}")
+    private var getUserByIdPort: Int? = null
+
+    @Value("\${http-get-user-by-id.endpoint}")
+    private lateinit var getUserByIdEndpoint: String
+
     @Singleton
-    fun userRepository(): UserRepository {
-        return FixedUserRepository()
+    fun userRepository(get: Get, objectMapper: ObjectMapper): UserRepository {
+        logger.debug("Initializing http get user by id with scheme: {}, host: {}, port: {}, endpoint: {}", getUserByIdScheme, getUserByIdHost, getUserByIdPort, getUserByIdEndpoint)
+
+        return HttpUserRepository(
+            get = get,
+            scheme = getUserByIdScheme,
+            host = getUserByIdHost,
+            port = getUserByIdPort!!,
+            endpoint = getUserByIdEndpoint,
+            objectMapper = objectMapper,
+        )
     }
 }
