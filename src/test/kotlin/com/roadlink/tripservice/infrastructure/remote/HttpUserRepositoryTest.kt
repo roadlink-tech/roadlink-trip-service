6e754cf1252247cf58825f92b9b135539134a063
jorge cabrera <cabrerajjorge@gmail.com>
2024-03-15 00:23:59 -0300

    feat: profile photo ulr (#28)

---
+++ src/test/kotlin/com/roadlink/tripservice/infrastructure/remote/HttpUserRepositoryTest.kt
@@ -0,0 +1,109 @@
+package com.roadlink.tripservice.infrastructure.remote
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+import com.roadlink.tripservice.dev_tools.infrastructure.network.Get
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+
+@MicronautTest
+class HttpUserRepositoryTest {
+
+    @Inject
+    private lateinit var objectMapper: ObjectMapper
+
+    private lateinit var wireMockServer: WireMockServer
+
+    private lateinit var userRepository: HttpUserRepository
+
+    @BeforeEach
+    fun setup() {
+        wireMockServer = WireMockServer(wireMockConfig().dynamicPort())
+        wireMockServer.start()
+
+        userRepository = HttpUserRepository(
+            get = Get(),
+            scheme = "http",
+            host = "localhost",
+            port = wireMockServer.port(),
+            endpoint = "user/{userId}",
+            objectMapper = objectMapper
+        )
+        configureFor("localhost", wireMockServer.port())
+    }
+
+
+    @AfterEach
+    fun tearDown() {
+        wireMockServer.stop()
+    }
+
+    @Test
+    fun `should return user when user exists`() {
+        // given
+        val userId = UUID.randomUUID()
+        stubFor(
+            get(urlEqualTo("/user/$userId"))
+                .willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(
+                            """
+                            {
+                                "id": "$userId",
+                                "first_name": "John",
+                                "last_name": "Doe",
+                                "email": "johndoe@example.com",
+                                "user_name": "johndoe",
+                                "profile_photo_url": "http://example.com/photo.jpg",
+                                "gender": "male"
+                            }
+                            """.trimIndent()
+                        )
+                        .withStatus(200)
+                )
+        )
+
+        // given
+        val user = userRepository.findByUserId(userId.toString())
+
+        // then
+        assertNotNull(user)
+        assertEquals(userId.toString(), user?.id)
+        assertEquals("John", user?.firstName)
+        assertEquals("Doe", user?.lastName)
+        assertEquals("http://example.com/photo.jpg", user?.profilePhotoUrl)
+    }
+
+    @Test
+    fun `should return null when user does not exist`() {
+        // given
+        val userId = UUID.randomUUID()
+        stubFor(
+            get(urlEqualTo("/user/$userId"))
+                .willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(
+                            """{}""".trimIndent()
+                        )
+                        .withStatus(404)
+                )
+        )
+
+        // given
+        val user = userRepository.findByUserId(userId.toString())
+
+        // then
+        assertNull(user)
+    }
+
+
+}
