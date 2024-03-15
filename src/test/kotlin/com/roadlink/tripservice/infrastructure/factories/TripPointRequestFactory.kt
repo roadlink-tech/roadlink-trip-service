419dd302f160b2fbf7fcfe9afc9d72877a8d2710
jorgecabrera <cabrerajjorge@gmail.com>
2024-03-14 23:39:50 -0300

    wip

--- src/test/kotlin/com/roadlink/tripservice/infrastructure/factories/TripPointRequestFactory.kt
+++ src/test/kotlin/com/roadlink/tripservice/infrastructure/factories/TripPointRequestFactory.kt
@@ -1,24 +1,24 @@
 package com.roadlink.tripservice.infrastructure.factories
 
+import com.roadlink.tripservice.infrastructure.rest.trip.request.TripPointRequest
 import com.roadlink.tripservice.usecases.factory.InstantFactory
-import com.roadlink.tripservice.infrastructure.requests.TripPointExpectedRequest
 import java.time.Instant
 
 object TripPointRequestFactory {
     fun avCabildo_4853() =
-        TripPointExpectedRequest(
+        TripPointRequest(
             estimatedArrivalTime = InstantFactory.october15_12hs().toString(),
             address = AddressRequestFactory.avCabildo_4853(),
         )
 
     fun avCabildo_20(estimatedArrivalTime: Instant = InstantFactory.october15_18hs()) =
-        TripPointExpectedRequest(
+        TripPointRequest(
             estimatedArrivalTime = estimatedArrivalTime.toString(),
             address = AddressRequestFactory.avCabildo_20(),
         )
 
     fun virreyDelPino_1800() =
-        TripPointExpectedRequest(
+        TripPointRequest(
             estimatedArrivalTime = InstantFactory.october15_17hs().toString(),
             address = AddressRequestFactory.virreyDelPino_1800(),
         )
--- src/test/kotlin/com/roadlink/tripservice/infrastructure/requests/CreateTripExpectedRequest.kt
+++ src/test/kotlin/com/roadlink/tripservice/infrastructure/requests/CreateTripExpectedRequest.kt
@@ -2,12 +2,13 @@ package com.roadlink.tripservice.infrastructure.requests
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.roadlink.tripservice.infrastructure.rest.trip.request.TripPointRequest
 
 data class CreateTripExpectedRequest(
     @JsonProperty(value = "driver") val driver: String,
     @JsonProperty(value = "vehicle") val vehicle: String,
-    @JsonProperty(value = "departure") val departure: TripPointExpectedRequest,
-    @JsonProperty(value = "arrival") val arrival: TripPointExpectedRequest,
-    @JsonProperty(value = "meeting_points") @JsonInclude(JsonInclude.Include.ALWAYS) val meetingPoints: List<TripPointExpectedRequest>,
+    @JsonProperty(value = "departure") val departure: TripPointRequest,
+    @JsonProperty(value = "arrival") val arrival: TripPointRequest,
+    @JsonProperty(value = "meeting_points") @JsonInclude(JsonInclude.Include.ALWAYS) val meetingPoints: List<TripPointRequest>,
     @JsonProperty(value = "available_seats") val availableSeats: Int,
 )
