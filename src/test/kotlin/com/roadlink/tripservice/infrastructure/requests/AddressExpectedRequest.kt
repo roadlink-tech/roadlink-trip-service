ba91aaf0b3b21ebddcc1a78e54e0293b5d0f961b
jorgecabrera <cabrerajjorge@gmail.com>
2024-03-14 23:36:20 -0300

    wip

--- src/test/kotlin/com/roadlink/tripservice/infrastructure/requests/AddressExpectedRequest.kt
+++ src/test/kotlin/com/roadlink/tripservice/infrastructure/requests/AddressExpectedRequest.kt
@@ -1,9 +1,10 @@
 package com.roadlink.tripservice.infrastructure.requests
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.roadlink.tripservice.infrastructure.rest.trip.request.LocationRequest
 
 data class AddressExpectedRequest(
-    @JsonProperty(value = "location") val location: LocationExpectedRequest,
+    @JsonProperty(value = "location") val location: LocationRequest,
     @JsonProperty(value = "street") val street: String,
     @JsonProperty(value = "city") val city: String,
     @JsonProperty(value = "country") val country: String,
