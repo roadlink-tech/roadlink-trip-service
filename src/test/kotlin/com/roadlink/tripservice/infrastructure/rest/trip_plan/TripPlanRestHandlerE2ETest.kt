f53eb469d936eb3b6c76b906b6a3457edc8430f5
mbosch <mbosch@whykeyway.com>
2024-03-25 22:36:42 -0300

    feat: retireve trip plan

--- src/test/kotlin/com/roadlink/tripservice/infrastructure/rest/trip_plan/TripPlanRestHandlerE2ETest.kt
+++ src/test/kotlin/com/roadlink/tripservice/infrastructure/rest/trip_plan/TripPlanRestHandlerE2ETest.kt
@@ -2,7 +2,6 @@ package com.roadlink.tripservice.infrastructure.rest.trip_plan
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.roadlink.tripservice.domain.trip_plan.TripPlan
-import com.roadlink.tripservice.domain.trip_plan.TripPlan.*
 import com.roadlink.tripservice.domain.trip_plan.TripPlan.Status.*
 import com.roadlink.tripservice.domain.trip_plan.TripPlanRepository
 import com.roadlink.tripservice.infrastructure.End2EndTest
@@ -12,6 +11,7 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.http.uri.UriBuilder
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
@@ -38,7 +38,7 @@ class TripPlanRestHandlerE2ETest : End2EndTest() {
         // given
         val userId = UUID.randomUUID()
         val savedTripPlan = givenASavedTripPlanWithPassengerId(userId)
-        val request: HttpRequest<Any> = givenARequest(userId)
+        val request: HttpRequest<Any> = givenAListTripPlanRequest(userId)
 
         entityManager.transaction.commit()
 
@@ -79,7 +79,7 @@ class TripPlanRestHandlerE2ETest : End2EndTest() {
         tripPlanRepository.insert(otherTripPlan)
         entityManager.transaction.commit()
 
-        val request: HttpRequest<Any> = givenARequest(userId)
+        val request: HttpRequest<Any> = givenAListTripPlanRequest(userId)
 
         // when
         val response = client.toBlocking().exchange(request, JsonNode::class.java)
@@ -247,15 +247,76 @@ class TripPlanRestHandlerE2ETest : End2EndTest() {
         )
     }
 
-    private fun givenARequest(userId: UUID?): HttpRequest<Any> =
+    /**
+     * Retrieve Trip Plan
+     */
+    @Test
+    fun `retrieve trip plan by id`() {
+        // given
+        val savedTripPlan = givenASavedTripPlan()
+        val request: HttpRequest<Any> = givenARetrievePlanRequest(savedTripPlan.id)
+        entityManager.transaction.commit()
+
+        // when
+        val response = client.toBlocking().exchange(request, JsonNode::class.java)
+
+        // then
+        assertEquals(HttpStatus.OK, response.status)
+        assertEquals(MediaType.APPLICATION_JSON_TYPE, response.contentType.get())
+        JSONAssert.assertEquals(
+            """
+           {
+              "id":"${savedTripPlan.id}",
+              "trip_legs":[
+                 {
+                    "id":"${savedTripPlan.tripLegs.first().id}",
+                    "trip_id":"${savedTripPlan.tripLegs.first().tripId}",
+                    "driver_id":"${savedTripPlan.tripLegs.first().driverId}",
+                    "vehicle_id":"${savedTripPlan.tripLegs.first().vehicleId}"
+                 }
+              ],
+              "passenger_id":"${savedTripPlan.passengerId}",
+              "status":"NOT_FINISHED"
+           }
+        """.trimIndent(), response.body().toString(), true
+        )
+    }
+
+    @Test
+    fun `retrieve trip plan by id, but do not match any result`() {
+        // given
+        val tripPlanId = UUID.randomUUID()
+        val request: HttpRequest<Any> = givenARetrievePlanRequest(tripPlanId)
+        entityManager.transaction.commit()
+
+        // when
+        val response = try { client.toBlocking().exchange(request, JsonNode::class.java) }
+            catch (e: HttpClientResponseException) { e.response }
+
+        // then
+        assertEquals(HttpStatus.NOT_FOUND, response.status)
+    }
+
+    private fun givenAListTripPlanRequest(userId: UUID?): HttpRequest<Any> =
         HttpRequest
             .GET(
                 UriBuilder.of("/trip-service/users/$userId/trip_plans")
                     .build()
             )
 
+    private fun givenARetrievePlanRequest(tripPlanId: UUID): HttpRequest<Any> =
+        HttpRequest
+            .GET(
+                UriBuilder.of("/trip-service/trip_plans/$tripPlanId")
+                    .build()
+            )
+
     private fun givenASavedTripPlanWithPassengerId(userId: UUID): TripPlan {
         tripPlanRepository.insert(TripPlanFactory.common(passengerId = userId)).also { return it }
+    }
+
+    private fun givenASavedTripPlan(): TripPlan {
+        tripPlanRepository.insert(TripPlanFactory.common()).also { return it }
     }
 
 }
