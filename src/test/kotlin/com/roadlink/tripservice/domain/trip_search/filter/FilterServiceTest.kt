d2cf5ce9a75adfc28618365a7e39bc08b9ab512d
jorgejcabrera <cabrerajjorge@gmail.com>
2024-05-10 11:44:27 -0300

    testing

--- src/test/kotlin/com/roadlink/tripservice/domain/trip_search/filter/FilterServiceTest.kt
+++ src/test/kotlin/com/roadlink/tripservice/domain/trip_search/filter/FilterServiceTest.kt
@@ -3,6 +3,7 @@ package com.roadlink.tripservice.domain.trip_search.filter
 import com.roadlink.tripservice.domain.trip.TripRepository
 import com.roadlink.tripservice.domain.trip.constraint.Policy
 import com.roadlink.tripservice.domain.trip.constraint.Restriction
+import com.roadlink.tripservice.domain.trip.constraint.Rule
 import com.roadlink.tripservice.domain.trip.constraint.Visibility
 import com.roadlink.tripservice.domain.user.User
 import com.roadlink.tripservice.usecases.trip.TripFactory
@@ -17,7 +18,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
-import java.util.stream.Stream
+import java.util.stream.*
 
 class FilterServiceTest {
 
@@ -45,8 +46,15 @@ class FilterServiceTest {
     ) {
         // given
         val trip = TripFactory.common(policies = policies, restrictions = restrictions)
-        val george = UserFactory.common(gender = passengerGender)
         val result = listOf(TripSearchPlanResultFactory.common(tripId = UUID.fromString(trip.id)))
+        val george = if (passengerAndDriverAreFriends) {
+            UserFactory.common(
+                gender = passengerGender,
+                friendsIds = setOf(UUID.fromString(trip.driverId))
+            )
+        } else {
+            UserFactory.common(gender = passengerGender)
+        }
         every { tripRepository.find(commandQuery = match { it.ids.contains(UUID.fromString(trip.id)) }) } returns listOf(
             trip
         )
@@ -62,7 +70,7 @@ class FilterServiceTest {
         @JvmStatic
         fun dataProvider(): Stream<Arguments> = Stream.of(
             Arguments.of(
-                "when the search returns a public trip without any rule or restrictions and the filters are empty, then it must not be filter",
+                "when the search returns a public trip without any rule or restrictions and the filters are empty, then it must not be filtered",
                 User.Gender.X,
                 false,
                 listOf<Policy>(),
@@ -71,13 +79,94 @@ class FilterServiceTest {
                 false
             ),
             Arguments.of(
-                "when the search returns a public trip for women without any rule or restrictions, the filters are empty and the caller is a woman, then it must not be filter",
+                "when the search returns a public trip for women without any rule, the filters are empty and the caller is a woman, then it must not be filtered",
                 User.Gender.Female,
                 false,
                 listOf<Policy>(),
                 listOf<Restriction>(Visibility.OnlyWomen),
                 listOf<Filter>(),
                 false
+            ),
+            Arguments.of(
+                "when the search returns a private trip for women without any rule, the filters are empty and the caller is a woman but not a driver fried, then it must be filtered",
+                User.Gender.Female,
+                false,
+                listOf<Policy>(),
+                listOf<Restriction>(Visibility.OnlyWomen, Visibility.Private),
+                listOf<Filter>(),
+                true
+            ),
+            Arguments.of(
+                "when the search returns a private trip for women without any rule, the filters are empty and the caller is a woman who is a driver fried, then it must not be filtered",
+                User.Gender.Female,
+                true,
+                listOf<Policy>(),
+                listOf<Restriction>(Visibility.OnlyWomen, Visibility.Private),
+                listOf<Filter>(),
+                false
+            ),
+            Arguments.of(
+                "when the search returns a private trip for women without any rule, the filters are empty and the caller is a men who is a driver fried, then it must be filtered",
+                User.Gender.Male,
+                true,
+                listOf<Policy>(),
+                listOf<Restriction>(Visibility.OnlyWomen, Visibility.Private),
+                listOf<Filter>(),
+                true
+            ),
+            Arguments.of(
+                "when the search returns a public trip which allow pets, the filter includes PET_ALLOWED, then it must not be filtered",
+                User.Gender.Male,
+                false,
+                listOf<Policy>(Rule.PetAllowed),
+                listOf<Restriction>(),
+                listOf(Filter.PET_ALLOWED),
+                false
+            ),
+            Arguments.of(
+                "when the search returns a public trip without any rule, the filter includes PET_ALLOWED, then it must be filtered",
+                User.Gender.Male,
+                false,
+                listOf<Policy>(),
+                listOf<Restriction>(),
+                listOf(Filter.PET_ALLOWED),
+                true
+            ),
+            Arguments.of(
+                "when the search returns a private trip which allow pets, the filter includes PET_ALLOWED but user and driver are not friends, then it must be filtered",
+                User.Gender.Male,
+                false,
+                listOf<Policy>(Rule.PetAllowed),
+                listOf<Restriction>(Visibility.Private),
+                listOf(Filter.PET_ALLOWED),
+                true
+            ),
+            Arguments.of(
+                "when the search returns a private trip which allow pets and does not allow smoke, the filter includes PET_ALLOWED and user is a driver friend, then it must not be filtered",
+                User.Gender.Male,
+                true,
+                listOf<Policy>(Rule.PetAllowed, Rule.NoSmoking),
+                listOf<Restriction>(Visibility.Private),
+                listOf(Filter.PET_ALLOWED),
+                false
+            ),
+            Arguments.of(
+                "when the search returns a public trip which allow pets and does not allow smoke, the filter includes PRIVATE, then it must be filtered",
+                User.Gender.Male,
+                true,
+                listOf<Policy>(Rule.PetAllowed, Rule.NoSmoking),
+                listOf<Restriction>(),
+                listOf(Filter.PRIVATE),
+                true
+            ),
+            Arguments.of(
+                "when the search returns a private trip for women which allow pets and does not allow smoke, the filter includes PRIVATE, ONLY_WOMEN, NO_SMOKE and ALLOW_PETS, then it must not be filtered",
+                User.Gender.Male,
+                true,
+                listOf<Policy>(Rule.PetAllowed, Rule.NoSmoking),
+                listOf<Restriction>(Visibility.Private, Visibility.OnlyWomen),
+                listOf(Filter.PRIVATE),
+                true
             ),
         )
     }
